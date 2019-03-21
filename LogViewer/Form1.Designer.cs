d267aa55edf07c4114e359a44f30d11edeb6dbbc
Łukasz Kawecki <kawec94@gmail.com>
2019-03-21 20:45:50 +0100

    Add project files.

---
+++ LogViewer/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace LogViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Open = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button_Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_Open
+            // 
+            this.button_Open.Location = new System.Drawing.Point(12, 12);
+            this.button_Open.Name = "button_Open";
+            this.button_Open.Size = new System.Drawing.Size(395, 31);
+            this.button_Open.TabIndex = 0;
+            this.button_Open.Text = "Open";
+            this.button_Open.UseVisualStyleBackColor = true;
+            this.button_Open.Click += new System.EventHandler(this.button_Open_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(789, 639);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button_Save
+            // 
+            this.button_Save.Location = new System.Drawing.Point(406, 12);
+            this.button_Save.Name = "button_Save";
+            this.button_Save.Size = new System.Drawing.Size(395, 32);
+            this.button_Save.TabIndex = 2;
+            this.button_Save.Text = "Save";
+            this.button_Save.UseVisualStyleBackColor = true;
+            this.button_Save.Click += new System.EventHandler(this.button_Save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 700);
+            this.Controls.Add(this.button_Save);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_Open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Open;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button_Save;
+    }
+}
+
---
+++ LogViewer/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LogViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Open_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Kawec\source\repos\LogViewer\LogViewer\LogViewerDatabase.mdf;Integrated Security = True");
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            var patch = ofd.FileName;
+            string[] lines = File.ReadLines(patch).ToArray();
+            string[][] splittedLines = new string[lines.Length][];
+            int length = 0;
+            for(int i = 0; i < lines.Length; i++)
+            {
+                splittedLines[i] = lines[i].Split(',');
+                if (splittedLines[i].Length > length) length = splittedLines[i].Length;
+            }
+            MessageBox.Show(length.ToString());
+            for(int i = 0; i < length; i++)
+            {
+                dataGridView1.Columns.Add("Column", "");
+            }
+            string[] row = new string[length];
+            for(int i = 0; i < splittedLines.Length; i++)
+            {
+                for (int j = 0; j < splittedLines[i].Length; j++)
+                {
+                    row[j] = splittedLines[i][j];
+                }
+                dataGridView1.Rows.Add(row);
+                Array.Clear(row, 0, row.Length);
+            }
+            DataTable dtFromGrid = new DataTable();
+            dtFromGrid = dataGridView1.DataSource as DataTable;
+
+        }
+
+        private void button_Save_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Kawec\source\repos\LogViewer\LogViewer\LogViewerDatabase.mdf;Integrated Security = True");
+            try
+            {
+                con.Open();
+                SqlCommand command;
+                command = new SqlCommand("DROP TABLE LogX", con);
+                //command.ExecuteNonQuery();
+                string dbString= "CREATE TABLE LogX(X(50), Y(50), Z(50));";
+                command = new SqlCommand(dbString, con);
+                command.ExecuteNonQuery();
+                con.Close();
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
